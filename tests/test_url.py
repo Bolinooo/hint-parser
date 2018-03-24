330e41f7fb7b7a29e8e05edbb51cd2b19ab63bfb
Bolinooo <0864947@hr.nl>
2018-03-24 20:09:38 +0100

    renamed unittest + changes

--- tests/test_url.py
+++ tests/test_url.py
@@ -4,9 +4,10 @@ import unittest
 
 class MyUrlTest(unittest.TestCase):
 
-    def test_build_sequence(self):
-        self.assertEqual(url.apply_format(3), "000003")
-        self.assertNotEqual(url.apply_format(3), "00003")
+    def test_apply_format(self):
+        self.assertNotEqual(url.apply_format(3), "000003")
+        self.assertEqual(url.apply_format(3), "00003")
+        self.assertNotEqual(url.apply_format(3), "0003")
 
 
 if __name__ == "__main__":
