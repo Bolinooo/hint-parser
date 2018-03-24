b8a9a2ce95a0333b2d17e80c5b400d29df3da326
Bolinooo <0864947@hr.nl>
2018-03-24 19:04:13 +0100

    update url

--- src/url.py
+++ src/url.py
@@ -2,6 +2,7 @@ from .decorator import *
 from .helper import *
 import requests
 
+cfg = get_config('config.ini')
 
 @validate_response
 def get_response(url):
@@ -12,14 +13,3 @@ def get_response(url):
     """
     return url
 
-
-def build_sequence(n):
-    """
-    Function to build sequence of strings for url
-    :param n: Number of strings to convert
-    :return: set of strings
-    """
-    seq = set()
-    for i in range(1, n + 1):
-        seq.add(apply_format(i))
-    return seq
