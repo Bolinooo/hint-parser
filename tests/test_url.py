7f32f2f93067f21d3f3fdad0b4ea1ad29ea9fe45
Bolinooo <0864947@hr.nl>
2018-03-24 19:04:46 +0100

    edit unittest test_url

--- tests/test_url.py
+++ tests/test_url.py
@@ -5,7 +5,7 @@ import unittest
 class MyUrlTest(unittest.TestCase):
 
     def test_build_sequence(self):
-        self.assertEqual(url.build_sequence(3),({"000001", "000002", "000003"}))
+        self.assertEqual(url.apply_format(3), "000003")
 
 
 if __name__ == "__main__":
