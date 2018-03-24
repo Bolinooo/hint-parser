7f6a10639444026d68ce55167105ea09816c8941
Bolinooo <0864947@hr.nl>
2018-03-24 19:06:13 +0100

    edit url unittest

--- tests/test_url.py
+++ tests/test_url.py
@@ -6,6 +6,7 @@ class MyUrlTest(unittest.TestCase):
 
     def test_build_sequence(self):
         self.assertEqual(url.apply_format(3), "000003")
+        self.assertNotEqual(url.apply_format(3), "00003")
 
 
 if __name__ == "__main__":
