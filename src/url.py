3a5faf687684feb95be8c8ea2572ddcc715fcf7d
Bolinooo <0864947@hr.nl>
2018-03-24 20:07:52 +0100

    added function for building set of available links

--- src/url.py
+++ src/url.py
@@ -15,15 +15,29 @@ def get_response(url):
 
 
 def build_set(option):
-
-    # http://misc.hro.nl/roosterdienst/webroosters/CMI/kw3/13/c/c00001.htm
+    """
+    Function to build set of links with status code 200 for defined option
+    :param option: Option from config-file: TEACHER, ROOM or COURSE
+    :return: set of links with given option
+    """
 
     base = cfg["URL"]["BASE"]
     quarter = cfg["SETTINGS"]["QUARTER"]
     education = cfg["SETTINGS"]["EDUCATION"]
     option = cfg["OPTIONS"][option]
 
-    url = base + education + "/kw" + quarter + "/"
+    base_url = base + education + "/kw" + quarter + "/"
+    links = set()
+    connected = True
 
-
-    return url
+    for week in range(13, 20, 1):
+        num = 1
+        while connected:
+            url = base_url + str(week) + "/" + option + "/" + option + apply_format(num) + ".htm"
+            resp = get_response("{0}".format(url))
+            if resp[1] != 200:
+                connected = False
+            else:
+                links.add(url)
+            num += 1
+    return links
