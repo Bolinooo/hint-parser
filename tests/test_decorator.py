ec5626d5a916021b2c778b34b42f734c336c1f37
Bolinooo <0864947@hr.nl>
2018-03-24 19:05:04 +0100

    renamed imports

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,4 +1,4 @@
-from src import core
+from src import main
 
 import requests
 import unittest
@@ -7,8 +7,8 @@ import unittest
 class MyDecoratorTests(unittest.TestCase):
 
     def test_validate_url(self):
-        self.assertEqual(core.get_response("https://www.google.nll"), None)
-        self.assertIsInstance(core.get_response("https://www.google.nl"), tuple)
+        self.assertEqual(main.get_response("https://www.google.nll"), None)
+        self.assertIsInstance(main.get_response("https://www.google.nl"), tuple)
 
 
 if __name__ == "__main__":
