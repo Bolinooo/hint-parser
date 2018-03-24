fd2dce68e6bb46153fff6e2af62474b4d9bd9512
Bolinooo <0864947@hr.nl>
2018-03-24 20:11:26 +0100

    changed parser.py with test call

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,11 @@
 from .url import *
-
+import requests
 
 def start():
-    # todo
-    print('test')
-    
+
+    # links_room = build_set("ROOM")
+    links_teacher = build_set("TEACHER")
+    for link in sorted(links_teacher):
+        print(link)
+
+
