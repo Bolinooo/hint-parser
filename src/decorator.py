89922ecfc3818dc5915993295330c9e75518fd06
Bolinooo <0864947@hr.nl>
2018-03-24 16:09:22 +0100

    update decorator.py

--- src/decorator.py
+++ src/decorator.py
@@ -6,13 +6,13 @@ def validate_response(func):
     """
     Decorator to check availability url
     :param func: get_response(url)-function
-    :return: If valid a 200 statuscode, if invalid None
+    :return: If valid tuple with response object and status_code, if invalid None
     """
     @functools.wraps(func)
     def newfunc(url):
         try:
             response = requests.get(url)
-            return response.status_code
+            return response, response.status_code
         except Exception as e:
             return None
     return newfunc
