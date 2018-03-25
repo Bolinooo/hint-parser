9fa8d8a02da55aecddffc6d18ad10c231c4cdb5f
Bolinooo <0864947@hr.nl>
2018-03-25 14:37:14 +0200

    update links crawler logic

--- src/url.py
+++ src/url.py
@@ -16,36 +16,46 @@ def get_response(url):
 
 def build_dict():
     """
-    Function to build dict of links with status code 200 for all options in config.ini
-    :return: set of links with given option
+    Function to build dict of links with status code 200
+    :return: dict of links for each option
     """
-
     links = {}
     connected = True
 
     for option in cfg.options("OPTIONS"):
-        for week in range(1, 52):
-            num = 1
-            while connected:
-                url = build_url(option, week, num)
-                resp = get_response("{0}".format(url))
-                if resp[1] != 200:
-                    break
-                else:
-                    if option not in links:
-                        links.setdefault(option, [])
-                    links[option].append(url)
-                num += 1
+        for quarter in range(1, 5):
+            for week in range(1, 53):
+                print("Checking for {0} in quarter {1} for week {2}".format(option, quarter, week))
+                num = 1
+                while connected:
+                    url = build_url(
+                        quarter=quarter,
+                        option=option,
+                        week=week,
+                        num=num
+                    )
+                    resp = get_response("{0}".format(url))
+                    if resp[1] != 200:
+                        break
+                    else:
+                        if option not in links:
+                            links.setdefault(option, [])
+                        links[option].append(url)
+                    num += 1
+    print("Succesfully build dicts with all available links in schedule.")
     return links
 
 
-def build_url(option, week, num):
-
+def build_url(**kwargs):
+    """
+    Function to build url based on given input
+    :param kwargs: dict with following keys: option, quarter, week and num
+    :return: constructed url string
+    """
     base = cfg["URL"]["BASE"]
-    quarter = cfg["SETTINGS"]["QUARTER"]
     education = cfg["SETTINGS"]["EDUCATION"]
-    option = cfg["OPTIONS"][option]
+    option = cfg["OPTIONS"][kwargs['option']]
 
-    base_url = base + education + "/kw" + quarter + "/"
+    base_url = base + education + "/kw" + str(kwargs['quarter']) + "/"
 
-    return base_url + str(week) + "/" + option + "/" + option + apply_format(num) + ".htm"
+    return base_url + str(kwargs['week']) + "/" + option + "/" + option + apply_format(kwargs['num']) + ".htm"
