adb24d335c17e141c923c377e7f32c681eed7efe
Bolinooo <0864947@hr.nl>
2018-03-25 14:42:30 +0200

    update test_url.py

--- tests/test_url.py
+++ tests/test_url.py
@@ -4,10 +4,8 @@ import unittest
 
 class MyUrlTest(unittest.TestCase):
 
-    def test_apply_format(self):
-        self.assertNotEqual(url.apply_format(3), "000003")
-        self.assertEqual(url.apply_format(3), "00003")
-        self.assertNotEqual(url.apply_format(3), "0003")
+    def test_build_dict(self):
+        self.assertEqual(url.build_dict(), type(dict))
 
 
 if __name__ == "__main__":
