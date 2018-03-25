4bd79306ab3f0ec0054be7cd94a5aed8454ff04d
Bolinooo <0864947@hr.nl>
2018-03-25 19:09:34 +0200

    setdefault changed, removed defaultdict

--- src/url.py
+++ src/url.py
@@ -1,6 +1,6 @@
 from .decorator import *
 from .helper import *
-from collections import defaultdict
+
 
 cfg = get_config('config.ini')
 
@@ -18,9 +18,9 @@ def get_response(url):
 def build_dict():
     """
     Function to build dict of links with status code 200
-    :return: defaultdict of links for each option
+    :return: dict of links for each option
     """
-    links = defaultdict(list)
+    links = {}
 
     for option in cfg.options("OPTIONS"):
         for quarter in range(1, 5):
@@ -38,6 +38,8 @@ def build_dict():
                     if resp[1] != 200:
                         break
                     else:
+                        if option not in links:
+                            links.setdefault(option, [])
                         links[option].append(url)
                     num += 1
     print("Succesfully build dicts with all available links in schedule.")
