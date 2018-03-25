6510b95ee1167fcac9b3e09d5c2eadefd93d4810
Bolinooo <0864947@hr.nl>
2018-03-25 14:42:43 +0200

    update test_helper.py

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,4 +1,5 @@
 from src import helper
+from src import url
 
 import unittest
 import configparser
@@ -9,6 +10,11 @@ class MyHelperTest(unittest.TestCase):
     def test_get_config(self):
         self.assertEqual(helper.get_config('config.inii'), None)
         self.assertIsInstance(helper.get_config('config.ini'), configparser.ConfigParser)
+
+    def test_apply_format(self):
+        self.assertNotEqual(url.apply_format(3), "000003")
+        self.assertEqual(url.apply_format(3), "00003")
+        self.assertNotEqual(url.apply_format(3), "0003")
 
 
 if __name__ == "__main__":
