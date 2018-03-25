9db4b0c5eb9ef1298a4d77e0a3140dc63cc5c0fd
Bolinooo <0864947@hr.nl>
2018-03-25 19:00:27 +0200

    update unittest

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,11 +1,12 @@
 from src import url
+from collections import defaultdict
 import unittest
 
 
 class MyUrlTest(unittest.TestCase):
 
     def test_build_dict(self):
-        self.assertIsInstance(url.build_dict(), dict)
+        self.assertIsInstance(url.build_dict(), defaultdict(list))
 
 
 if __name__ == "__main__":
