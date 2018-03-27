8dc996827ba5f3870bf7212a23785e93f80b6f2d
Alex <0887185@hr.nl>
2018-03-27 14:24:36 +0200

    not rly working faster url

--- src/url.py
+++ src/url.py
@@ -25,26 +25,35 @@ def build_dict():
     :return: dict of links for each option
     """
     links = defaultdict(list)
-
+    unavailableSchedules = defaultdict(list)
+    # skip quarters and weeks that are empty after the first iteration to prefvent repetition
     for option in cfg.options("OPTIONS"):
-        for quarter in range(1, 5):
-            for week in range(1, 53):
-                print("Checking for {0} in quarter {1} for week {2}".format(option, quarter, week))
-                num = 1
-                while True:
-                    url = build_url(
-                        quarter=quarter,
-                        option=option,
-                        week=week,
-                        num=num
-                    )
-                    resp = get_response("{0}".format(url))
-                    if resp[1] != 200:
-                        break
-                    else:
-                        links[option].append(url)
-                    num += 1
+        if option == "c":
+            print("c")
+        for quarter in range(1, 3):
+            for week in range(1, 47):
+                # ex skip till kw1 week 45
+                # {kw: [1,2,3,4]}
+                if not((quarter in unavailableSchedules) & (week in unavailableSchedules[quarter])):
+                    print("Checking for {0} in quarter {1} for week {2}".format(option, quarter, week))
+                    num = 1
+                    while True:
+                        url = build_url(
+                            quarter=quarter,
+                            option=option,
+                            week=week,
+                            num=num
+                        )
+                        resp = get_response("{0}".format(url))
+                        if resp[1] != 200:
+                            unavailableSchedules[quarter].append(week)
+                            break
+                        else:
+                            links[option].append(url)
+                        num += 1
+            print("just for debugging")
     print("Succesfully build dicts with all available links in schedule.")
+    print(links)
     return links
 
 
