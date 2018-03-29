6c47afab7e550f9c9b87d669f10f2e2eff880d85
Bolinooo <0864947@hr.nl>
2018-03-29 10:11:57 +0200

    edit main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 from .parser import *
-
+from .url import *
 
 def main():
 
-    item = parse()
-    for nr, i in sorted(enumerate(item)):
-        print(i)
+    link = "http://misc.hro.nl/roosterdienst/webroosters/CMI/kw3/14/t/t00050.htm"
+
+
+    resp = requests.get(link)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+
+    convert_dates(soup)
+
+    # item = parse()
+
+    #
+    #
+    #
+    # for nr, i in sorted(enumerate(item)):
+    #     print(i)
 
     # links = build_dict()
+
     # extracted_links = [[i for i in links[key]] for key in links.keys()]
 
     # for link in links.values():
