1a40ed52659129c2e8de0623527fd0bdc5a2ae16
Bolinooo <0864947@hr.nl>
2018-03-29 16:52:49 +0200

    fix unittest url

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,13 +1,17 @@
 from src import url
+from src import helper
 from collections import defaultdict
 
 import unittest
+
+cfg = helper.get_config('config.ini')
+sections = cfg.options('OPTIONS')
 
 
 class MyUrlTest(unittest.TestCase):
 
     def test_build_urls(self):
-        self.assertEqual(url.build_urls('TEACHER'), defaultdict(list))
+        self.assertEqual(url.build_urls(sections[0]), defaultdict(list))
 
 
 if __name__ == "__main__":
