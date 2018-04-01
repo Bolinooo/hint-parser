645a971d8a0dff82ae5b0e2ebe63a8d26b2902ec
Bolinooo <0864947@hr.nl>
2018-04-01 17:36:49 +0200

    improved comments in main function

--- src/main.py
+++ src/main.py
@@ -9,28 +9,29 @@ def main():
     Main flow of the parser
     1) Crawl all available links
     2) Parse all the valid responses
-    3) ...
+    3) Store length of list for each option
+    4) Convert them to dict
     """
 
     start_time = time.time()
 
-    # Crawl all available links using threads
+    # Step 1) Crawl all available links
     pool = ThreadPool(4)
     links = pool.map(build_urls, cfg.options('OPTIONS'))
 
-    # Map the parse()-function over each response from each option
+    # Step 2) Parse all the valid responses
     parsed_items = []
-    counters = {}
-
     for dd in links:
         for k, v in dd[0].items():
             parsed_items.append([parse(resp[0]) for resp in v])
 
+    # Step 3) Store length of list for each option
+    counters = {}
     for dd in links:
         for k, v in dd[1].items():
-            counters[k] = len(v)
+            counters[k] = (len(v), v)
 
-
+    # Step 4) TODO = Convert them to dict (work in progress)
     # convert_dicts(parsed_items, data)
 
     print("--- %s seconds ---" % (time.time() - start_time))
