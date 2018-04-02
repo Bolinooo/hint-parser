b06d485ae74d0e2e3b6754e37310c70fd66ea62d
Bolinooo <0864947@hr.nl>
2018-04-02 18:41:43 +0200

    added unittest to test_helper.py

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,11 +1,15 @@
 from src import helper
 from src import url
 
+from datetime import datetime
 import unittest
 import configparser
 
 
 class MyHelperTest(unittest.TestCase):
+
+    current = datetime.now()
+    time_format = current.strftime('%d-%m')
 
     def test_get_config(self):
         self.assertEqual(helper.get_config('config.inii'), None)
@@ -15,6 +19,9 @@ class MyHelperTest(unittest.TestCase):
         self.assertNotEqual(url.apply_format(3), "000003")
         self.assertEqual(url.apply_format(3), "00003")
         self.assertNotEqual(url.apply_format(3), "0003")
+
+    def test_build_filename(self):
+        self.assertEqual(helper.build_filename(), 'schedule_' + self.__class__.time_format)
 
 
 if __name__ == "__main__":
