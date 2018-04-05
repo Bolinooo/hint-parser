1a72fcaa8bddf2b2d6281bf049574a75be60cfb0
Alex <0887185@hr.nl>
2018-04-05 12:23:32 +0200

    main uncommented

--- src/main.py
+++ src/main.py
@@ -13,41 +13,40 @@ def main():
     4) Convert them to dict
     5) Parse final dict to a json
     """
-    data = get_response("http://misc.hro.nl/roosterdienst/webroosters/CMI/kw3/14/t/t00005.htm")
-    parse(data[0])
-    # start_time = time.time()
-    #
-    # # Step 1) Crawl all available links
-    # threads = ThreadPool(2)
-    # data = threads.map(build_responses, cfg.options('OPTIONS'))
-    #
-    # parsed_items = []
-    # parsed_counters = {}
-    #
-    # for dd in data: # dd = defaultdict
-    #     # Step 2) Parse all the responses
-    #     for option, responselist in dd[0].items():
-    #         parsed_counters[option] = len(responselist)
-    #         parsed_items.append([parse(resp[0]) for resp in responselist])
-    #         print("Succesfully parsed all responses for {option}".format(option=option))
-    #     # Step 3) Store length of list for each option
-    #     for option, timedata in dd[1].items():
-    #         if len(timedata) == parsed_counters[option]:
-    #             parsed_counters[option] = (len(timedata), timedata)
-    #             print("Succesfully stored additional data [quarter, week] for {option}".format(option=option))
-    #
-    # # Step 4) Convert them to dict
-    # final = compare_dicts(parsed_items, parsed_counters)
-    #
-    # # Step 5) Parse final dict to a json
-    # options = [
-    #     'building',
-    #     'floor',
-    #     'room'
-    # ]
-    # convert_csv(final, options, skip_empty=True, unique=True)
-    #
-    # print("Parser is finished. It took {seconds} seconds.".format(seconds=round(time.time() - start_time)))
+
+    start_time = time.time()
+
+    # Step 1) Crawl all available links
+    threads = ThreadPool(2)
+    data = threads.map(build_responses, cfg.options('OPTIONS'))
+
+    parsed_items = []
+    parsed_counters = {}
+
+    for dd in data: # dd = defaultdict
+        # Step 2) Parse all the responses
+        for option, responselist in dd[0].items():
+            parsed_counters[option] = len(responselist)
+            parsed_items.append([parse(resp[0]) for resp in responselist])
+            print("Succesfully parsed all responses for {option}".format(option=option))
+        # Step 3) Store length of list for each option
+        for option, timedata in dd[1].items():
+            if len(timedata) == parsed_counters[option]:
+                parsed_counters[option] = (len(timedata), timedata)
+                print("Succesfully stored additional data [quarter, week] for {option}".format(option=option))
+
+    # Step 4) Convert them to dict
+    final = compare_dicts(parsed_items, parsed_counters)
+
+    # Step 5) Parse final dict to a json
+    options = [
+        'building',
+        'floor',
+        'room'
+    ]
+    convert_csv(final, options, skip_empty=True, unique=True)
+
+    print("Parser is finished. It took {seconds} seconds.".format(seconds=round(time.time() - start_time)))
 
     # teacher = 3504
     # classes = 2534
