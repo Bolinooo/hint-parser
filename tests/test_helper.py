46cc8c86943041580c6b63d61e6924da0e301bbf
Bolinooo <0864947@hr.nl>
2018-06-14 11:06:40 +0200

    Removed unused line

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -9,8 +9,6 @@ class MyHelperTest(unittest.TestCase):
 
     timestamp = calendar.timegm(time.gmtime())
 
-    test_dict = {"key1": {"key2": {"key3": "value1"}}}
-
     def setUp(self):
         print("Testing method: {0}".format(self._testMethodName))
 
