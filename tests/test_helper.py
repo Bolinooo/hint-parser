b4e09ef3ae730d3d5ca0c46c5b05b94be9e26e82
Bolinooo <0864947@hr.nl>
2018-06-14 10:53:21 +0200

    Added unit tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -22,8 +22,25 @@ class MyHelperTest(unittest.TestCase):
         self.assertEqual(url.apply_format(3), "00003")
         self.assertNotEqual(url.apply_format(3), "0003")
 
-    def test_build_filename(self):
-        self.assertEqual(helper.build_filename(option="teacher", quarter="1"), 'teacher_q1_' + str(self.__class__.timestamp))
+    def test_build_filename_teacher(self):
+        self.assertNotEqual(helper.build_filename(option="teacher", quarter="1"), 'teacher_q2_' + str(self.__class__.timestamp))
+        self.assertEqual(helper.build_filename(option="teacher", quarter="2"), 'teacher_q2_' + str(self.__class__.timestamp))
+        self.assertNotEqual(helper.build_filename(option="teacher", quarter="3"), 'teacher_q2_' + str(self.__class__.timestamp))
+
+    def test_build_filename_rooms(self):
+        self.assertNotEqual(helper.build_filename(option="rooms", quarter="1"), 'rooms_q2_' + str(self.__class__.timestamp))
+        self.assertEqual(helper.build_filename(option="rooms", quarter="2"), 'rooms_q2_' + str(self.__class__.timestamp))
+        self.assertNotEqual(helper.build_filename(option="rooms", quarter="3"), 'rooms_q2_' + str(self.__class__.timestamp))
+
+    def test_build_filename_schedule(self):
+        self.assertNotEqual(helper.build_filename(option="schedule", quarter="1"), 'schedule_q2_' + str(self.__class__.timestamp))
+        self.assertEqual(helper.build_filename(option="schedule", quarter="2"), 'schedule_q2_' + str(self.__class__.timestamp))
+        self.assertNotEqual(helper.build_filename(option="schedule", quarter="3"), 'schedule_q2_' + str(self.__class__.timestamp))
+
+    def test_build_filename_classes(self):
+        self.assertNotEqual(helper.build_filename(option="classes", quarter="1"), 'classes_q2_' + str(self.__class__.timestamp))
+        self.assertEqual(helper.build_filename(option="classes", quarter="2"), 'classes_q2_' + str(self.__class__.timestamp))
+        self.assertNotEqual(helper.build_filename(option="classes", quarter="3"), 'classes_q2_' + str(self.__class__.timestamp))
 
 
 if __name__ == "__main__":
